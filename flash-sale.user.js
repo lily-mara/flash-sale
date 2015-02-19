528e813ef6da9606910112bbea0059245071c6ae
Nate Mara <natemara@gmail.com>
2015-02-19 13:39:20 -0500

    add jquery to script

--- flash-sale.user.js
+++ flash-sale.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name        Steam Flash Sale
-// @namespace   com.natemara.flash-sale
-// @description Quickly sell your steam items
-// @include     /https?://(www\.)?steamcommunity\.com/market/.*/
-// @version     3
-// @grant       none
+// @name		Steam Flash Sale
+// @namespace	com.natemara.flash-sale
+// @description	Quickly sell your steam items
+// @include		/https?://(www\.)?steamcommunity\.com/market/.*/
+// @version		3
+// @grant		none
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // ==/UserScript==
 
+var $ = unsafeWindow.jQuery;
 var itemPattern = /Starting at: $[0-9.]+/;
 
 function getPrice() {
 	var matches = document.documentElement.innerHTML.match(itemPattern);
-	var itemPrice = document.getElementById("iteminfo1_item_market_actions");
-	console.log(matches);
+	var itemPrice = $("iteminfo1_item_market_actions");
+	console.log(itemPrice);
 }
 
 setInterval(function() {
