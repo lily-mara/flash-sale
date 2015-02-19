50d6401be980958655faf5e606b82f2ec708a454
Nate Mara <natemara@gmail.com>
2015-02-19 13:23:51 -0500

    search page with regex

--- flash-sale.user.js
+++ flash-sale.user.js
@@ -7,9 +7,12 @@
 // @grant       none
 // ==/UserScript==
 
+var itemPattern = /Starting at: $[0-9.]+/;
+
 function getPrice() {
+	var matches = document.documentElement.innerHTML.match(itemPattern);
 	var itemPrice = document.getElementById("iteminfo1_item_market_actions");
-	window.alert(itemPrice);
+	window.alert(matches);
 }
 
 setInterval(function() {
