6667b32ef9aef779b64b4533cd5f7bb6daaccc71
Nate Mara <natemara@gmail.com>
2015-02-19 13:06:01 -0500

    fix regex for page

--- flash-sale.user.js
+++ flash-sale.user.js
@@ -2,17 +2,18 @@
 // @name        Steam Flash Sale
 // @namespace   com.natemara.flash-sale
 // @description Quickly sell your steam items
-// @include     /https?://(www\.)?steamcommunity\.com/market/.*
+// @include     /https?://(www\.)?steamcommunity\.com/market/.*/
 // @version     2
 // @grant       none
 // ==/UserScript==
 
 function getPrice() {
-	var items = document.getElementById("iteminfo1_item_market_actions");
-	alert(items);
+	var itemPrice = document.getElementById("iteminfo1_item_market_actions");
+	if (itemPrice) {
+		window.alert(itemPrice);
+	}
 }
 
 setInterval(function() {
 	getPrice();
-	window.alert("hello");
-}, 1000);
+}, 5000);
