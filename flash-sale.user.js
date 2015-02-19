a38358de4740af9ee7568dca80c6f01221522cc9
Nate Mara <natemara@gmail.com>
2015-02-19 12:57:21 -0500

    add basic user script

---
+++ flash-sale.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Steam Flash Sale
+// @namespace   com.natemara.flash-sale
+// @description Quickly sell your steam items
+// @include     /https?://(www\.)?steamcommunity\.com/market/.*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function getPrice() {
+	var items = document.getElementById("iteminfo1_item_market_actions");
+	alert(items);
+}
+
+setInterval(function() {
+	getPrice();
+}, 1000);
